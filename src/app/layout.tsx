5767b091849bfcf3298ebb0e7c18d2ea38413908
Zubiarrain <zubiarrainnahuel@gmail.com>
2024-06-09 14:51:47 -0300

    fix: cache de imagenes 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,45 @@ import Navbar from "@/components/Navbar";
 import TransitionProvider from "@/components/TransitionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
+const APP_NAME = "NZ Portfolio App";
+const APP_DEFAULT_TITLE = "NZ Portfolio App";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "The best animated portfolio page";
 
 export const metadata: Metadata = {
-  title: "NZ Portfolio App",
-  description: "The best animated portfolio page",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
 
